cd8f0a88472555fe410f2e75b354f37e09fc8f37
Sadia Mubasshira <rahmanmubasshira@gmail.com>
2021-11-17 00:39:56 +0600

    new java class added

---
+++ app/src/main/java/com/project/newsapp/zeeniea.java
@@ -0,0 +1,17 @@
+package com.project.newsapp;
+
+public class zeeniea {
+    int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public zeeniea(int a) {
+        this.a = a;
+    }
+}
